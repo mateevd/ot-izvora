e45ae79a3af3834bed2f88116fc5b834ade5acd5
Darin Mateev <mateevd@gmail.com>
2019-10-07 13:18:42 +0200

    add routing + components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes, Router, ActivatedRoute} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule} from '@angular/material';
+
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { HomeComponent } from './home/home.component';
+import { EventComponent } from './event/event.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'event', component: EventComponent},
+    { path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent,
+    MainNavComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
