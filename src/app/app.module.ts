77696bb82dc89ebbc0f849622f7efcc9856f1e37
Darin Mateev <mateevd@gmail.com>
2019-10-07 14:47:08 +0200

    add grid and cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes, Router, ActivatedRoute} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule} from '@angular/material';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule} from '@angular/material';
+import {MatGridListModule} from '@angular/material/grid-list';
 
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { HomeComponent } from './home/home.component';
-import { EventComponent } from './event/event.component';
-import { ContactComponent } from './contact/contact.component';
+import {MainNavComponent} from './main-nav/main-nav.component';
+import {HomeComponent} from './home/home.component';
+import {EventComponent} from './event/event.component';
+import {ContactComponent} from './contact/contact.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent},
-    { path: 'event', component: EventComponent},
-    { path: 'contact', component: ContactComponent},
+    {path: '', component: HomeComponent},
+    {path: 'event', component: EventComponent},
+    {path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EventComponent,
-    MainNavComponent,
-    HomeComponent,
-    ContactComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    RouterModule.forRoot(routes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EventComponent,
+        MainNavComponent,
+        HomeComponent,
+        ContactComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatCardModule,
+        MatGridListModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 
-export class AppModule {}
+export class AppModule {
+}
--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-event',
-  templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+    selector: 'app-event',
+    templateUrl: './event.component.html',
+    styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    title = 'Events from FB';
+    events = [
+        {title: 'Event 1', description: 'qsdmlqsmldqsmldkmqs', cols: 2, rows: 1},
+        {title: 'Event 2', description: 'qsdmlqsmldqsmldkmqs', cols: 2, rows: 1},
+        {title: 'Event 3', description: 'qsdmlqsmldqsmldkmqs', cols: 2, rows: 1},
+        {title: 'Event 4', description: 'qsdmlqsmldqsmldkmqs', cols: 2, rows: 1},
+    ];
+
+    ngOnInit() {
+    }
 
 }
